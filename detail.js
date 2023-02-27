5cb86187242a65c2af7834921cfde06f79401b9d
solda <principe1513@gmail.com>
2023-02-27 22:48:46 +0900

    이벤트버블링 & data-set

--- detail.js
+++ detail.js
@@ -4,11 +4,20 @@
 // - 모든 div에 붙은 show 클래스명 제거
 // - div0에  show 클래스명 추가
 
+let list = document.querySelector('.list');
+list.addEventListener('click', function(e) {
+    let num = Number(e.target.dataset.id);
+    tabOpen(num);
+});
+
 let count = document.querySelectorAll('.tab-button').length
 console.log(count);
 
-for(let i = 0; i < count; i++) {
-    document.querySelectorAll('.tab-button')[i].addEventListener('click', function() {
+// for(let i = 0; i < count; i++) {
+//     tabOpen(i);
+// }
+function tabOpen(i) {
+    // document.querySelectorAll('.tab-button')[i].addEventListener('click', function() {
             document.querySelectorAll('.tab-button').forEach((el) => {
                 el.classList.remove('orange');
             })
@@ -18,5 +27,5 @@ for(let i = 0; i < count; i++) {
             
         document.querySelectorAll('.tab-button')[i].classList.add('orange');
         document.querySelectorAll('.tab-content')[i].classList.add('show');
-    });
+    // });
 }
